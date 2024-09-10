ab37b485190365eecf4926fabde6953ed4b58c9e
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-09-09 23:50:50 -0300

    refactor: pages

--- src/pages/favoritesPage/index.tsx
+++ src/pages/favoritesPage/index.tsx
@@ -1,5 +1,41 @@
+import { DrugCard } from "@/components/drugCard";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useAppContext } from "@/shared/context/AppContext";
+
 function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  const { favorites, addFavorite, removeFavorite } = useAppContext();
+  return (
+    <ScrollArea className="h-[75vh] md:mx-6 mt-6 p-3 rounded-lg flex justify-center bg-white">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center">
+        {favorites &&
+          favorites.map((drug) => (
+            <DrugCard
+              key={drug.id}
+              isFavorite={favorites.some((fav) => fav.id === drug.id)}
+              brand_name={drug.openfda.brand_name[0]}
+              generic_name={
+                drug.openfda.generic_name ? drug.openfda.generic_name[0] : "N/A"
+              }
+              manufacturer_name={
+                drug.openfda.manufacturer_name
+                  ? drug.openfda.manufacturer_name[0]
+                  : "N/A"
+              }
+              active_ingredient={
+                drug.active_ingredient ? drug.active_ingredient[0] : "N/A"
+              }
+              route={drug.openfda.route ? drug.openfda.route[0] : "N/A"}
+              drugInfo={drug}
+              toggleFunction={() =>
+                favorites.some((fav) => fav.id === drug.id)
+                  ? removeFavorite(drug.id)
+                  : addFavorite(drug)
+              }
+            />
+          ))}
+      </div>
+    </ScrollArea>
+  );
 }
 
 export { FavoritesPage };
