1beb876d5bb66c4243984229c37ef2244ae2d356
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-09-09 23:49:37 -0300

    feat: create layout

--- src/components/headerUserActions/index.tsx
+++ src/components/headerUserActions/index.tsx
@@ -1,22 +1,19 @@
+import { useAppContext } from "@/shared/context/AppContext";
 import { UserRound } from "lucide-react";
 
-type HeaderUserActionsProps = {
-  userName?: string;
-};
-
-function HeaderUserActions({ userName }: HeaderUserActionsProps) {
+function HeaderUserActions() {
+  const { clearStorage } = useAppContext();
   return (
-    <div className="hidden md:flex items-center gap-2 min-w-[8.5rem] hover:cursor-pointer mr-8 hover:scale-105 transition-transform duration-300">
+    <div
+      onClick={() => clearStorage()}
+      className="hidden md:flex items-center gap-2 min-w-[8.5rem] hover:cursor-pointer mr-8 hover:scale-105 transition-transform duration-300"
+    >
       <div className="flex justify-center items-center h-10 w-10 shrink-0 overflow-hidden rounded-full bg-muted">
         <UserRound className="w-5 h-5" />
       </div>
       <span>
-        <p className="text-xs">{`Olá, ${
-          userName ? userName : "Bem-vindo!"
-        }`}</p>
-        <p className="text-sm text-yellow-primary">
-          {userName ? "Sair" : "Entrar"}
-        </p>
+        <p className="text-xs">Olá Bem-vindo!</p>
+        <p className="text-sm text-yellow-primary">refresh</p>
       </span>
     </div>
   );
